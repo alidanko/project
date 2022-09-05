4f61dc866e0ffaea61c64c179b6ae80ff36bc914
Meder <nmnm9490@gmail.com>
2022-09-05 16:04:51 +0600

    add h3

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
-'use strict';
+// 'use strict';
 
-var number = 5;
-var string = "Hello World";
-var sym = Symbol();
-var boolean = true;
-null;
-undefined;
-var obj = {};
+// var number = 5;
+// var string = "Hello World";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
 
-console.log(4/0);
-console.log('string'*9);
-let something;
-console.log(something);
+// console.log(4/0);
+// console.log('string'*9);
+// let something;
+// console.log(something);
 
-let persone = {
-name: "John",
-age: 250,
-isMarried: false
-}
+// let persone = {
+// name: "John",
+// age: 250,
+// isMarried: false
+// }
 
-console.log(persone["name"]);
+// console.log(persone["name"]);
 
-let arr = ['plum.png','orange.jpg','apple.bmp'];
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
 
-console.log(arr[0]);
-// alert("Hello World!");
+// console.log(arr[0]);
+// // alert("Hello World!");
 
-// let answer = confirm("Are yuo here?");
-// console.log(answer);
+// // let answer = confirm("Are yuo here?");
+// // console.log(answer);
 
-// let answer = +prompt("Есть ли вам 18?", "Да");
-// ujyhujgh
-// console.log(typeof(answer));
+// // let answer = +prompt("Есть ли вам 18?", "Да");
+// // ujyhujgh
+// // console.log(typeof(answer));
 
-// console.log("arr" + " - object");
-// console.log(4 + +" - object");
+// // console.log("arr" + " - object");
+// // console.log(4 + +" - object");
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
 
 
-console.log(incr++);
-console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
 
-let isChecked = true,
-    isClose = false;
+// let isChecked = true,
+//     isClose = false;
 
-console.log(isChecked || !isClose);
+// console.log(isChecked || !isClose);
